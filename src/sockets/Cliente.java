dd7a92583e729185a9782b67bd062f0b5043e369
braiso-22 <braisfv22@gmail.com>
2022-01-13 17:20:28 +0100

    primer commit, cliente y servidor funcionan en local

---
+++ src/sockets/Cliente.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sockets;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ *
+ * @author brais.fernandezvazqu
+ */
+public class Cliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FrameCliente ventana1 = new FrameCliente();
+
+        
+
+    }
+
+}
